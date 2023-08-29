d68f31ed9528967a7cfe27960916faebada2d704
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-29 09:35:44 +0200

    Learning

--- Java/Core/hello.java
+++ Java/Core/hello.java
@@ -4,8 +4,15 @@ class Hello
     {
         int num = 3;
         int num2 = 10;
+        int result = num + num2;
+
+        float mark = 6.5f;
+
+        double mark2 = 6.5f;
 
         System.out.println("Hello Africa");
-        System.out.println(num + num2);
+        System.out.println(result);
+        System.out.println(mark);
+        System.out.println(mark2);
     }
 }
