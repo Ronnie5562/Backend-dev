b2994e225111ee5416f9ec53c37c65965fa5054e
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-30 15:41:28 +0200

    Learning

---
+++ Java/Core/if_else.java
@@ -0,0 +1,20 @@
+public class if_else
+{
+
+    public static void main(String[] args)
+    {
+        int first = 10;
+        int second = 20;
+        int third = 30;
+    
+        if (first > 15){
+            System.out.println("We're good. The first value is greater than 15");
+        } else if (second > 15) {
+            System.out.println("We're good. The second value is greater than 15");
+        } else if (third > 15) {
+            System.out.println("Almost screwed!! but thankfully, the third value is greater than 15");
+        } else {
+            System.out.println("Shit, Our trials failed ");
+        }
+    }
+}
