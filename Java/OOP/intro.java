22c3dbe3e8da3f37f0b06bef8a911293b39e6ca8
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-31 21:12:41 +0200

    Learning

--- Java/OOP/intro.java
+++ Java/OOP/intro.java
@@ -1,5 +1,25 @@
 class Calc {
-    // Created a new class
+    // A calculator class
+
+    public int add(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public int sub(int num1, int num2) {
+        return (num1 - num2);
+    }
+
+    public int mul(int num1, int num2) {
+        return (num1 * num2);
+    }
+
+    public float div(int num1, int num2) {
+        return (num1 / num2);
+    }
+    
+    public int mod(int num1, int num2) {
+        return (num1 + num2);
+    }
 }
 
 public class intro {
