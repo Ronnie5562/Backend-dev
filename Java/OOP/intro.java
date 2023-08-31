768aef2f34a5c0e6711cf4783044018c678581e1
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-08-31 21:23:39 +0200

    Learning

--- Java/OOP/intro.java
+++ Java/OOP/intro.java
@@ -1,4 +1,4 @@
-class Calc {
+class Calculator {
     // A calculator class
 
     public int add(int num1, int num2) {
@@ -14,17 +14,35 @@ class Calc {
     }
 
     public float div(int num1, int num2) {
-        return (num1 / num2);
+        return ((float)num1 / (float)num2);
     }
-    
+
     public int mod(int num1, int num2) {
-        return (num1 + num2);
+        return (num1 % num2);
     }
 }
 
 public class intro {
     // Intro to OOP in Java
     public static void main(String[] args){
-        System.out.println("Hello Class");
+        Calculator calculator = new Calculator();
+
+        int num1 = 15;
+        int num2 = 4;
+
+        int sum = calculator.add(num1, num2);
+        System.out.println("Sum: " + sum);
+
+        int difference = calculator.sub(num1, num2);
+        System.out.println("Difference: " + difference);
+
+        int product = calculator.mul(num1, num2);
+        System.out.println("Product: " + product);
+
+        float quotient = calculator.div(num1, num2);
+        System.out.println("Quotient: " + quotient);
+
+        int remainder = calculator.mod(num1, num2);
+        System.out.println("Remainder: " + remainder);
     }
 }
