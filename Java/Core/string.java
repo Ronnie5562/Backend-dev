6f538a77fa09778fe969121cb1c86e9b23a0f4e6
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-11 17:34:33 +0200

    Learning

--- Java/Core/string.java
+++ Java/Core/string.java
@@ -15,5 +15,22 @@ public class string {
 
 
         //  NOTE: By default, strings in Java are immutable but if you want to get to use mutable strings, you can use - The StringBuffer or StringBuilder class in java
+        // 1. StringBuffer
+
+        StringBuffer fullName = new StringBuffer("Abimbola");
+        System.out.println(fullName.capacity());
+        System.out.println(fullName.length());
+
+        // Capacity is the num of chars that the string has. NOTE: It has 16 extra chars space. In this case, "Rollybobo" has 9 chars plus the default 16 chars, that makes it 25.
+
+
+        // So, Let's say I made a mistake with my fullName by adding only my lastName without my firstName
+        // Let's update it
+
+        fullName.append(" Ronald");
+        System.out.println(fullName);
+
+        //.append() is just on of the many methods that StringBuffer provides us. You can explore others based on your needs.
+
     }
 }
