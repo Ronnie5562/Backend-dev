30c7de01c20d632af0a0ab07c2a6aae8e93fb2d0
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-12 17:20:16 +0200

    Learning

--- Java/OOP/Learn_Interface.java
+++ Java/OOP/Learn_Interface.java
@@ -1,25 +1,60 @@
-interface Computer {
+interface Gadgets {
+    
+}
+
+interface Devices {
+    void OFF();
+    void ON();
+}
+
+
+interface Computer extends Devices {
     void build();
 }
 
 
-class Desktop implements Computer {
+
+class Desktop implements Computer, Gadgets { // Interfaces, sort of forces the concept of multiple inheritance into Java which is not originally there !!!
     public void build(){
         System.out.println("Code, Compile, Run, Test, Deploy !!! ==> Faster");
     }
+
+    public void ON(){
+        System.out.println("Booting device (Desktop) .....");
+        System.out.println("Successfully Logged in ");
+    }
+
+    public void OFF(){
+        System.out.println("Logging out .....");
+        System.out.println("Switching off .....");
+    }
+
 }
 
 
-class Laptop implements Computer {
+class Laptop implements Computer, Gadgets {
     public void build(){
         System.out.println("Code, Compile, Run, Test, Deploy !!!");
     }
+
+    public void ON(){
+        System.out.println("Booting device (Laptop) .....");
+        System.out.println("Successfully Logged in ");
+    }
+
+    public void OFF(){
+        System.out.println("Logging out .....");
+        System.out.println("Switching off .....");
+    }
+
 }
 
 
 class Developer {
     public void devApp(Computer lap){
+        lap.ON();
         lap.build();
+        lap.OFF();
     }
 }
 
