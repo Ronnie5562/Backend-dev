37dcd6e6e29f535e9ab86c204308dfea72af84d1
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-12 11:40:19 +0200

    Learning

--- Java/Core/wrapper_class.java
+++ Java/Core/wrapper_class.java
@@ -2,5 +2,17 @@
 // The wrapper class are Integer, Double e.t.c
 
 public class wrapper_class {
-    String 
+   
+    public static void main(String[] args){
+        String num = "12";
+
+        // The below will not work because we cannot typecast an obj to a primitive type. and that's where wrapper classes come in - they help us bridge the gap.
+        // int num1 = (int) num;
+
+        // We can do this instead
+        int num2 = Integer.parseInt(num);
+
+        System.out.println(num2 * 5);
+
+    }
 }
