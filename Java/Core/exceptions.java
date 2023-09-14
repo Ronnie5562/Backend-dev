2d1e983a3970fab47ebe859c0d083dea93506650
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-14 21:08:02 +0200

    Learning ✅

--- Java/Core/exceptions.java
+++ Java/Core/exceptions.java
@@ -1,3 +1,8 @@
+/*
+ * Exception are basically runtime errors.
+ * In Java, the Exception class inherits from another class called Throwable. ==> Fun fact: Most keywords in java that ends eith the suffix "able" are interfaces, but Throwable is an actaul class and not an interface
+ */
+
 @FunctionalInterface
 interface doMath {
     int divide(int a, int b);
@@ -12,34 +17,40 @@ class solveMath implements doMath{
 public class exceptions {
     public static void main(String[] args){
         int a = 15;
-        int b = 3;
+        int b = 0;
 
         solveMath Student = new solveMath();
         int nums[] = new int[5];
 
-        for (int i = 0; i < 5; i++){
-            nums[i] = i + 1;
-        }
+        int arrayNum = 0;
+        double result = 0.0;
+
 
         try
         {
-            double result = (double) Student.divide(a, b);
-            int arrayNum = nums[4];
-            System.out.println(result);
-            System.out.println(arrayNum);
+            result = (double) Student.divide(a, b);
+
+            for (int i = 0; i < 5; i++){
+                nums[i] = i + 1;
+            }
+            arrayNum = nums[4];
         }
         catch (ArithmeticException error)
         {
-            System.out.println("You cannot divide with a 0 (zero) as your denominator");
+            System.out.println("You cannot divide with a 0 (zero) as your denominator ==> By default, we will divide it by 1");
+            result = (double) Student.divide(a, 1);
         }
         catch (ArrayIndexOutOfBoundsException error)
         {
-            System.out.println("You are trying to get a value from an index that doesn't exist. There highest index in nums is:" + (nums.length - 1));
+            System.out.println("You are trying to get / set a value from an index that doesn't exist. There highest index in nums is:" + (nums.length - 1));
         }
         catch (Exception error)
         {
             System.out.println("Something went wrong, check your code techbro isonu");
         }
+
+        System.out.println(result);
+        System.out.println(arrayNum);
 
     }
 }
