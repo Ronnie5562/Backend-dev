d4528493157762dde68e86d4a77fcb880f82961c
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-18 15:41:14 -0600

    Learning ✅

--- Java/Core/getInput.java
+++ Java/Core/getInput.java
@@ -4,8 +4,13 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class getInput {
-    public static void main(String[] args){
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException{ // In the real world, you should not throw an exception from your `main` method. You're literally telling JVM to handle the exception lol.
         System.out.print("Enter an integer: ");
+        BufferedReader bfreader = null;
 
         
         try {
@@ -15,7 +20,7 @@ public class getInput {
 
             //                 Method 2
             InputStreamReader in = new InputStreamReader(System.in);
-            BufferedReader bfreader = new BufferedReader(in);
+            bfreader = new BufferedReader(in);
 
             int num = Integer.parseInt(bfreader.readLine());
             System.out.println(num); //The default output is the ASCII value of the integer the usr inputs. You can solve this error by subtracting 48 from the actual value because te ASCII value of number starts from 48(where 0=48).
@@ -25,6 +30,10 @@ public class getInput {
         }
         catch (Exception error){
             System.out.println("An unknown Exception(Runtime error) occured");
+        }
+        finally {
+            System.out.println("I will win");
+            bfreader.close();
         }
 
         //                   Method 3 (Using Scanner) ==> Best Approach
