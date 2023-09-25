ff7ff6503a20b9ec5f6835bfd99c886845a8d87f
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-25 23:28:31 +0200

    Learning ✅

--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
@@ -1,8 +1,10 @@
 package com.ronnie5562;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,18 +14,23 @@ public class AddServlet extends HttpServlet {
 	
 // We used service() method below -> It works for both post and get request from the form.
 // We also have other methods like doGet() and doPost() that only accepts request from a it's methods. e.g => doGet accepts only requests with a `get` method
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
 	{
 		int firstNum = Integer.parseInt(req.getParameter("num1"));
 		int secondNum = Integer.parseInt(req.getParameter("num2"));
 		
 		int result = firstNum + secondNum;
 		
-		PrintWriter out = res.getWriter();
-		out.println("The result is: " + result);
-
+//		PrintWriter out = res.getWriter();
+//		out.println("The result is: " + result);
+//
+//		
+//		System.out.println("The result is: " + result);
 		
-		System.out.println("The result is: " + result);
+		req.setAttribute("result", result);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("square");
+		rd.forward(req, res);
 	}
 }
  
--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/SqServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/SqServlet.java
@@ -1,7 +1,21 @@
 package com.ronnie5562;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 public class SqServlet extends HttpServlet {
-
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int result = (int)req.getAttribute("result");
+		result *= result;
+		PrintWriter out = res.getWriter();
+		
+		out.println("From Sq Servlet");
+		out.println("Result: " + result);
+	}
 }
