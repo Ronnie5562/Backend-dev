c033fca230810fae9d2e5b50c7bc11b618e702ab
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-25 15:33:50 +0200

    Learning ✅

--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
@@ -1,5 +1,18 @@
 package com.ronnie5562;
 
-public class AddServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res)
+	{
+		int firstNum = Integer.parseInt(req.getParameter("num1"));
+		int secondNum = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = firstNum + secondNum;
+		
+		System.out.println(result);
+	}
 }
+ 
