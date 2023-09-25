dc18dc4e3553cf84d4930a142c9a3471b9a55211
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-25 15:44:40 +0200

    Learning ✅

--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res)
 	{
 		int firstNum = Integer.parseInt(req.getParameter("num1"));
@@ -12,7 +14,7 @@ public class AddServlet extends HttpServlet {
 		
 		int result = firstNum + secondNum;
 		
-		System.out.println(result);
+		System.out.println("The result is: " + result);
 	}
 }
  
