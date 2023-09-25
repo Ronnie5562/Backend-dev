75c80e7ae9c71b81e33aa159c1d0a9675dc4ffa5
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-25 15:56:24 +0200

    Learning ✅

--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/AddServlet.java
@@ -1,5 +1,8 @@
 package com.ronnie5562;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,12 +10,16 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 //	private static final long serialVersionUID = 1L;
 
-	public void service(HttpServletRequest req, HttpServletResponse res)
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
 		int firstNum = Integer.parseInt(req.getParameter("num1"));
 		int secondNum = Integer.parseInt(req.getParameter("num2"));
 		
 		int result = firstNum + secondNum;
+		
+		PrintWriter out = res.getWriter();
+		out.println("The result is: " + result);
+
 		
 		System.out.println("The result is: " + result);
 	}
