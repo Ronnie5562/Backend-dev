1be443dd80213aaf1c8d53c1c51c452120276cdb
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-26 16:58:41 +0200

    Learning Servlet Cookies ✅

--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/authenticate_login.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/authenticate_login.java
@@ -1,5 +1,6 @@
 package com.ronnie5562;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,6 +8,25 @@ import jakarta.servlet.http.HttpServletResponse;
 public class authenticate_login extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res)
 	{
+		Cookie[] MyCookies = req.getCookies();
+		String email = "";
+		String pass = "";
+		
+		for (Cookie cookie : MyCookies)
+		{
+			if (cookie.getName().equals("email"))
+				email = cookie.getValue();
+			if (cookie.getName().equals("pass"))
+				pass = cookie.getValue();
+		}
+		
+		String serverEmail = "r.abimbola@alustudent.com";
+		String serverPassword = "admin";
+		
+		
+		
+		if ((email == serverEmail) && (pass == serverPassword))
+			System.out.println();
 		
 	}
 }
--- Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/loginServlet.java
+++ Java EE/Jsp_Servlets/DemoApp/src/main/java/com/ronnie5562/loginServlet.java
@@ -1,17 +1,25 @@
 package com.ronnie5562;
 
+import java.io.IOException;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class loginServlet extends HttpServlet {
-	public void service(HttpServletRequest req, HttpServletResponse res)
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
 		String email = req.getParameter("email");
 		String pass = req.getParameter("pass");
 		
 		
-		Cookie email_cookie = new Cookie("email", "");
+		Cookie email_cookie = new Cookie("email", email);
+		res.addCookie(email_cookie);
+		
+		Cookie pass_cookie = new Cookie("pass", pass);
+		res.addCookie(pass_cookie);
+		
+		res.sendRedirect("authenticate");
 	}
 }
