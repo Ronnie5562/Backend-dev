a98be28e715add3bb420027bec437afffa1e0c76
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-07 21:02:46 +0200

    JSTL

--- Java EE/Jstl/tutorial/src/main/java/com/ronnie5562/DemoServlet.java
+++ Java EE/Jstl/tutorial/src/main/java/com/ronnie5562/DemoServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/DemoServlet")
 public class DemoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = "Ronald";
