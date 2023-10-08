595b691dfc9f01d129ed13c1031146e31cd80e7f
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-08 14:11:38 +0200

    Servlet Filter

---
+++ Java EE/Jstl/tutorial/src/main/java/com/ronnie5562/idFilter.java
@@ -0,0 +1,38 @@
+package com.ronnie5562;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpFilter;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class idFilter
+ */
+public class idFilter extends HttpFilter implements Filter {
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
