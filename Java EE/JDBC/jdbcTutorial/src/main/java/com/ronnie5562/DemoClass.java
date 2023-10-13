cdacb8a14b6860f50c4ed9065c473830b95db99e
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-14 01:06:32 +0200

    Learning JDBC

---
+++ Java EE/JDBC/jdbcTutorial/src/main/java/com/ronnie5562/DemoClass.java
@@ -0,0 +1,19 @@
+package com.ronnie5562;
+
+/* ==> To successfully work with JDBC, you have to follow the seven steps below
+ * 1. import the needed packages
+ * 2. Load and Register the driver
+ * 3. Create a connection.
+ * 4. Create a statement
+ * 5. Execute the query
+ * 6. Process the result
+ * 7. Close the connection 
+ */
+
+public class DemoClass {
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
