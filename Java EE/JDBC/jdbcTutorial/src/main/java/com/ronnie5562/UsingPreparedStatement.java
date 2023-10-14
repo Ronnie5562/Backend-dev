bba1e0ec2d5a1ff9d2d052ebff5ec1c5e5e63499
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-14 10:25:23 +0200

    Learning JDBC

--- Java EE/JDBC/jdbcTutorial/src/main/java/com/ronnie5562/UsingPreparedStatement.java
+++ Java EE/JDBC/jdbcTutorial/src/main/java/com/ronnie5562/UsingPreparedStatement.java
@@ -7,13 +7,23 @@ public class UsingPreparedStatement {
 	{
 		String url = "jdbc:mysql://localhost:3306/sakila";
 		String uname = "root";
+		int rollNo = 203;
+		String firstName = "Sharon";
+		String lastName = "Abimbola";
+		String lastUpdated = "2023-10-14 05:02:19";
 		String pass = ""; // Remember to add your database password before you run !!!
-		String query = "INSERT INTO actor VALUES (202, 'Ronald', 'Abimbola', '2023-10-14 05:02:19');";
+		String query = "INSERT INTO actor VALUES (?, ?, ?, ?);";
 		
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url, uname, pass);
-		Statement st = con.createStatement();
-		int count = st.executeUpdate(query);
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, rollNo);
+		st.setString(2, firstName);
+		st.setString(3, lastName);
+		st.setString(4, lastUpdated);
+		
+		
+		int count = st.executeUpdate();
 		
 		if (count == 1)
 		{
