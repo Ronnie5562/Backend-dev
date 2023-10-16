de6d90213214cf2248cbbc842e351552535edf47
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-16 17:10:00 +0200

    Building projects

--- Java EE/Projects/mvc-jsp-servlet-project/src/main/java/com/ronnie5562/mvc/model/Employee.java
+++ Java EE/Projects/mvc-jsp-servlet-project/src/main/java/com/ronnie5562/mvc/model/Employee.java
@@ -1,5 +1,40 @@
 package com.ronnie5562.mvc.model;
 
 public class Employee {
-
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
