bd948da33a6bbaa1407a9de398f49e37565e1664
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-16 17:37:48 +0200

    Building projects

---
+++ Java EE/Projects/mvc-jsp-servlet-project/src/main/java/com/ronnie5562/mvc/controller/EmployeeServlet.java
@@ -0,0 +1,29 @@
+package com.ronnie5562.mvc.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Webservlet("/EmployeeServlet")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+    public EmployeeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
