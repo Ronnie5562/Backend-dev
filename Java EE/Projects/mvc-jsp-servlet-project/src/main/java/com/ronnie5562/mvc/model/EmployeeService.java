524a6c97ce2220a4b64538f0d1e0d5350cde1f4c
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-16 17:28:54 +0200

    Building projects

--- Java EE/Projects/mvc-jsp-servlet-project/src/main/java/com/ronnie5562/mvc/model/EmployeeService.java
+++ Java EE/Projects/mvc-jsp-servlet-project/src/main/java/com/ronnie5562/mvc/model/EmployeeService.java
@@ -1,5 +1,13 @@
 package com.ronnie5562.mvc.model;
 
-public class EmployeeService {
+import java.util.*;
 
+public class EmployeeService {
+	public List<Employee> getEmployees() {
+		return Arrays.asList(new Employee(001, "Steve", "Rogers"),
+				new Employee(002, "Tony", "Stark"),
+				new Employee(003, "Thor", "Odinson"),
+				new Employee(004, "Carol", "Danvers"),
+				new Employee(005, "T'Challa", ""));
+	}
 }
