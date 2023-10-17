98e1b6e010c176834797580f2d7d38c646d07f59
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-17 23:07:04 +0200

    Building projects

--- Java EE/Projects/mvc-jsp-servlet/src/main/java/com/ronnie5562/mvc/controller/EmployeeServlet.java
+++ Java EE/Projects/mvc-jsp-servlet/src/main/java/com/ronnie5562/mvc/controller/EmployeeServlet.java
@@ -1,5 +1,6 @@
 package com.ronnie5562.mvc.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,19 +8,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.ronnie5562.mvc.model.EmployeeService;
 
+
+@WebServlet("/EmployeeServlet")
 public class EmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private EmployeeService employeeService;
 
     public EmployeeServlet() {
         super();
+        
+        this.employeeService = new EmployeeService();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		processResquest(request, response);
 	}
 
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		processResquest(request, response);
+	}
+	
+	private void processResquest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("employess", this.employeeService.getEmployees());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/employees.jsp");
+		dispatcher.forward(request, response);
+		
+		
 	}
 }
