87919236fe88a1fc283a5e665f5db210081ed833
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-19 22:23:49 +0200

    Building

--- Java EE/Projects/mvc-jsp-servlet/src/main/java/com/ronnie5562/mvc/model/EmployeeService.java
+++ Java EE/Projects/mvc-jsp-servlet/src/main/java/com/ronnie5562/mvc/model/EmployeeService.java
@@ -1,21 +1,34 @@
 package com.ronnie5562.mvc.model;
 
+import java.sql.*;
 import java.util.*;
+
+import com.ronnie5562.mvc.dao.EmployeeDao;
 
 public class EmployeeService {
 	public List<Employee> getEmployees() {
-//		ResultSet rs = st.executeQuery(query);
-//		while(rs.next())
-//		{
-//			String Fname = rs.getString("first_name");
-//			String Lname = rs.getString("last_name");
-//			
-//			System.out.println(Fname + " : " + Lname);
-//		}
+		EmployeeDao employeedao = new EmployeeDao();
+		ResultSet rs = employeedao.fetchEmployees();
 		
-		return Arrays.asList(new Employee(001, "Ronnie", "Starboy"),
-				new Employee(002, "Tony", "Stark"),
-				new Employee(003, "Tom", "Cruise"),
-				new Employee(004, "T'chala", "Panther"));
+		
+		List<Employee> list = new ArrayList<Employee>();
+	
+		
+		try {
+			
+			while(rs.next())
+			{
+				int id = rs.getInt("id");
+				String Fname = rs.getString("first_name");
+				String Lname = rs.getString("last_name");
+				
+				list.add(new Employee(id, Fname, Lname));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 }
