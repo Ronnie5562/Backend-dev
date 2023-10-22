259521b6117d766de8bdc67b5bbe8f09466daca2
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-22 10:49:53 +0200

    -- Added a new router that specifies where all /new request are sent to using doGet --

--- Java EE/CRUD_Application/jsp-servlet-jdbc-crud-app/src/main/java/com/starboy/usermanagement/web/UserServlet.java
+++ Java EE/CRUD_Application/jsp-servlet-jdbc-crud-app/src/main/java/com/starboy/usermanagement/web/UserServlet.java
@@ -1,6 +1,8 @@
 package com.starboy.usermanagement.web;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,6 +39,28 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getServletPath();
+		
+		switch (action) {
+			case "/new" -> showNewForm(request, response);
+			
+			case "/insert" -> System.out.println("heelo");
+			
+			case "/delete" -> System.out.println("heelo");
+			
+			case "/edit" -> System.out.println("heelo");
+			
+			case "/update" -> System.out.println("heelo");
+			
+			default -> throw new IllegalArgumentException("Unexpected value: " + action);
+			
+		}
+	}
+	
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
