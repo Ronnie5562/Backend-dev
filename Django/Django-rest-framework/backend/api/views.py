4b412be3a5a4206c55f8d6089f23db727b42a2fe
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-03 17:24:43 +0200

    Building an API

--- Django/Django-rest-framework/backend/api/views.py
+++ Django/Django-rest-framework/backend/api/views.py
@@ -1,18 +1,64 @@
-from django.http import JsonResponse
-import json
+# from django.http import JsonResponse
+# import json
+
+# from products.models import Product
+
+
+# def api_home(request, *args, **kwargs):
+
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+
+#     if model_data:
+#         data["id"] = model_data.id
+#         data["title"] = model_data.title
+#         data["content"] = model_data.content
+#         data["price"] = model_data.price
+
+#     return JsonResponse(data)
+
+##
+
+# from django.forms.models import model_to_dict
+# from django.http import JsonResponse
+# import json
+
+# from products.models import Product
+
+
+# def api_home(request, *args, **kwargs):
+
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+
+#     if model_data:
+#         data = model_to_dict(model_data, fields=["id", "title", "price"])
+
+#     return JsonResponse(data)
+
+
+# Using django-rest-framework
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
+
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
 
-    model_data = Product.object.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    
-    if model_data:
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-        
 
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(instance, fields=["id", "title", "price"])
+        data = ProductSerializer(instance).data
+
+    return Response(data)
